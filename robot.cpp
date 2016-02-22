83026e888d455788494a586bb3e8ff658589d84f
nishadprabhu <prabhunishad@yahoo.com>
2016-02-22 10:52:29 -0500

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -1,14 +1,19 @@
+//Including FEH libraries
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
 #include <FEHMotor.h>
 #include <FEHRPS.h>
+
+//Defining states for following lines
 #define ON_LINE 1.9
 #define CENTER 0
 #define LEFT -1
 #define FAR_LEFT -2
 #define RIGHT 1
 #define FAR_RIGHT 2
+#define OFF_LINE 4
+//Defining constants to convert counts to inches or degrees
 #define COUNTS_PER_INCH 40.5
 #define COUNTS_PER_DEGREE 2.389
 //Declarations for encoders & motors
@@ -22,6 +27,11 @@ AnalogInputPin right(FEHIO::P1_2);
 AnalogInputPin middle(FEHIO::P1_4);
 AnalogInputPin left(FEHIO::P1_6);
 AnalogInputPin cds(FEHIO::P1_0);
+
+AnalogInputPin frontLeftBump(FEHIO::PX_X);
+AnalogInputPin frontRightBump(FEHIO::PX_X);
+AnalogInputPin backLeftBump(FEHIO::PX_X);
+AnalogInputPin backRightBump(FEHIO::PX_X);
 void move_forward(int percent, float inches) //using encoders
 {
     //Reset encoder counts
@@ -29,16 +39,53 @@ void move_forward(int percent, float inches) //using encoders
     left_encoder.ResetCounts();
     float counts = inches*COUNTS_PER_INCH;
     //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(percent);
-
-
+	right_motor.SetPercent(percent);
+	left_motor.SetPercent(percent);
 
     //While the average of the left and right encoder are less than counts,
     //keep running motors
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
     //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+void move_backwards(int percent, float inches) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    float counts = inches*COUNTS_PER_INCH;
+    //Set both motors to desired percent
+	right_motor.SetPercent(-1*percent);
+	left_motor.SetPercent(-1*percent);
+
+    //While the average of the left and right encoder are less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+//Drives either forwards or backwards until it hits a wall.
+void driveToWall(int percent) {
+	//Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+	
+	right_motor.SetPercent(percent);
+	left_motor.SetPercent(percent);
+
+	if(percent > 0) {
+		while(frontLeftBump.Value() && frontRightBump.Value());
+	}
+	else {
+		while(backLeftBump.Value() && backRightBump.Value());
+	}
+	
+	
+	//Turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
@@ -65,43 +112,35 @@ void followLine(float speed) {
             else if(midValue <= ON_LINE && rightValue<= ON_LINE && leftValue >= ON_LINE) {
                 state = FAR_RIGHT;
             }
+			else {
+				state = OFF_LINE;
+			}
 
             switch(state) {
                 case CENTER:
                     left_motor.SetPercent(speed);
                     right_motor.SetPercent(speed);
-                    LCD.Write(leftValue);
-                    LCD.Write(rightValue);
-                    LCD.Write(midValue);
                     break;
                 case LEFT:
                     left_motor.SetPercent(speed);
                     right_motor.SetPercent(.75*speed);
-                    LCD.Write(leftValue);
-                    LCD.Write(rightValue);
-                    LCD.Write(midValue);
                     break;
                 case FAR_LEFT:
                     left_motor.SetPercent(speed);
                     right_motor.SetPercent(.5*speed);
-                    LCD.Write(leftValue);
-                    LCD.Write(rightValue);
-                    LCD.Write(midValue);
                     break;
                 case RIGHT:
                     left_motor.SetPercent(.75*speed);
                     right_motor.SetPercent(speed);
-                    LCD.Write(leftValue);
-                    LCD.Write(rightValue);
-                    LCD.Write(midValue);
                     break;
                 case FAR_RIGHT:
                     left_motor.SetPercent(.5*speed);
                     right_motor.SetPercent(speed);
-                    LCD.Write(leftValue);
-                    LCD.Write(rightValue);
-                    LCD.Write(midValue);
                     break;
+				case OFF_LINE;
+					left_motor.SetPercent(speed);
+                    right_motor.SetPercent(speed);
+					break;
             }
         }
 
@@ -133,12 +172,24 @@ void turn_right(int percent, float degrees) //using encoders
 
     while((right_encoder.Counts() + left_encoder.Counts())/2 < counts);
 
-
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
-//Runs through performance test 1
+void waitForStart() {
+	while(!buttons.MiddlePressed()); //Wait for middle button to be pressed
+    while(buttons.MiddlePressed()); //Wait for middle button to be unpressed
+	while(cds.Value() > 1);
+}
+//returns 1 if light is BLUE, RED if otherwise
+int getLightColor() {
+	if(cds.Value() > 1.5) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 void performance1() {
 
     Sleep(2.0); //Wait for counts to stabilize
@@ -146,12 +197,15 @@ void performance1() {
     Sleep(1.0);
     turn_right(20, 45);
     Sleep(1.0);
-    move_forward(30, 23);
+    driveToWall(30);
     Sleep(1.0);
+	move_backwards(20, 2);
+	Sleep(1.0);
     turn_left(20, 90);
     Sleep(1.0);
-    move_forward(30, 27 );
+    driveToWall(30);
     Sleep(1.0);
+	move_backwards(30, 2);
     turn_left(20, 90);
     left_motor.SetPercent(30);
     right_motor.SetPercent(30);
@@ -164,11 +218,8 @@ void performance1() {
 
 int main(void)
 {
-
-    while(!buttons.MiddlePressed()); //Wait for middle button to be pressed
-    while(buttons.MiddlePressed()); //Wait for middle button to be unpressed
+	waitForStart();
     performance1();
-
 
     return 0;
 }
