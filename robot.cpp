0fa15771799850e85abbaee20dcb6c1b16576e8e
nishadprabhu <prabhunishad@yahoo.com>
2016-02-25 12:13:01 -0500

    Update robot.cpp
    Made robot drive straighter, refined performance test method, robot should now detect lights

--- robot.cpp
+++ robot.cpp
@@ -15,11 +15,16 @@
 #define OFF_LINE 4
 //Defining constants to convert counts to inches or degrees
 #define COUNTS_PER_INCH 33.74
-#define COUNTS_PER_DEGREE 1.99
+#define COUNTS_PER_DEGREE 1.9444
+//Define thresholds for line following/start light
+#define START_LIGHT_ON 1.5
+#define BLUE_LIGHT_ON 1
+//Tuning constant
+#define K X
 //Declarations for encoders & motors
 ButtonBoard buttons(FEHIO::Bank3);
-DigitalEncoder right_encoder(FEHIO::P0_0);
-DigitalEncoder left_encoder(FEHIO::P0_1);
+DigitalEncoder right_encoder(FEHIO::P0_1);
+DigitalEncoder left_encoder(FEHIO::P0_2);
 FEHMotor right_motor(FEHMotor::Motor1,12.0);
 FEHMotor left_motor(FEHMotor::Motor0,12.0);
 
@@ -28,10 +33,10 @@ AnalogInputPin middle(FEHIO::P1_4);
 AnalogInputPin left(FEHIO::P1_6);
 AnalogInputPin cds(FEHIO::P1_0);
 
-AnalogInputPin frontLeftBump(FEHIO::PX_X);
-AnalogInputPin frontRightBump(FEHIO::PX_X);
-AnalogInputPin backLeftBump(FEHIO::PX_X);
-AnalogInputPin backRightBump(FEHIO::PX_X);
+DigitalInputPin frontLeftBump(FEHIO::P2_0);
+DigitalInputPin frontRightBump(FEHIO::P2_1);
+AnalogInputPin backLeftBump(FEHIO::P0_0);
+AnalogInputPin backRightBump(FEHIO::P0_0);
 void move_forward(int percent, float inches) //using encoders
 {
     //Reset encoder counts
@@ -39,12 +44,14 @@ void move_forward(int percent, float inches) //using encoders
     left_encoder.ResetCounts();
     float counts = inches*COUNTS_PER_INCH;
     //Set both motors to desired percent
-	right_motor.SetPercent(percent);
-	left_motor.SetPercent(percent);
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
 
     //While the average of the left and right encoder are less than counts,
     //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts) {
+        right_motor.SetPercent(K*(left_encoder.Counts()-right_encoder.Counts())+percent);
+    }
 
     //Turn off motors
     right_motor.Stop();
@@ -57,8 +64,8 @@ void move_backwards(int percent, float inches) //using encoders
     left_encoder.ResetCounts();
     float counts = inches*COUNTS_PER_INCH;
     //Set both motors to desired percent
-	right_motor.SetPercent(-1*percent);
-	left_motor.SetPercent(-1*percent);
+    right_motor.SetPercent(-1*percent);
+    left_motor.SetPercent(-1*percent);
 
     //While the average of the left and right encoder are less than counts,
     //keep running motors
@@ -70,28 +77,19 @@ void move_backwards(int percent, float inches) //using encoders
 }
 //Drives either forwards or backwards until it hits a wall.
 void driveToWall(int percent) {
-	//Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
-	
-	right_motor.SetPercent(percent);
-	left_motor.SetPercent(percent);
 
-	if(percent > 0) {
-		while(frontLeftBump.Value() && frontRightBump.Value());
-	}
-	else {
-		while(backLeftBump.Value() && backRightBump.Value());
-	}
-	
-	
-	//Turn off motors
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+    LCD.WriteLine(frontLeftBump.Value());
+    LCD.WriteLine(frontRightBump.Value());
+    while(frontLeftBump.Value() && frontRightBump.Value());
+    //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
 }
 void followLine(float speed) {
         int leftValue, rightValue, midValue, state;
-        while( true )
+        while(cds.Value() )
         {
             leftValue = left.Value();
             rightValue = right.Value();
@@ -112,9 +110,9 @@ void followLine(float speed) {
             else if(midValue <= ON_LINE && rightValue<= ON_LINE && leftValue >= ON_LINE) {
                 state = FAR_RIGHT;
             }
-			else {
-				state = OFF_LINE;
-			}
+            else {
+                state = OFF_LINE;
+            }
 
             switch(state) {
                 case CENTER:
@@ -137,10 +135,10 @@ void followLine(float speed) {
                     left_motor.SetPercent(.5*speed);
                     right_motor.SetPercent(speed);
                     break;
-				case OFF_LINE;
-					left_motor.SetPercent(speed);
+                case OFF_LINE:
+                    left_motor.SetPercent(speed);
                     right_motor.SetPercent(speed);
-					break;
+                    break;
             }
         }
 
@@ -176,20 +174,38 @@ void turn_right(int percent, float degrees) //using encoders
     right_motor.Stop();
     left_motor.Stop();
 }
+void turn(int percent, int counts) {
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    right_motor.SetPercent(-1 * percent);
+    left_motor.SetPercent(percent);
+
+    while((right_encoder.Counts() + left_encoder.Counts())/2 < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
 void waitForStart() {
-	while(!buttons.MiddlePressed()); //Wait for middle button to be pressed
+    while(!buttons.MiddlePressed()); //Wait for middle button to be pressed
     while(buttons.MiddlePressed()); //Wait for middle button to be unpressed
-	while(cds.Value() > 1);
+    while(cds.Value() > START_LIGHT_ON);
 }
 //returns 1 if light is BLUE, RED if otherwise
 int getLightColor() {
-	if(cds.Value() > 1.5) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+    if(cds.Value() > BLUE_LIGHT_ON) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
+bool detectingLight() {
+    return (cds.Value() < 2.75);
+}
+
 void performance1() {
 
     Sleep(2.0); //Wait for counts to stabilize
@@ -199,27 +215,37 @@ void performance1() {
     Sleep(1.0);
     driveToWall(30);
     Sleep(1.0);
-	move_backwards(20, 2);
-	Sleep(1.0);
+    move_backwards(20, 1);
+    Sleep(1.0);
     turn_left(20, 90);
     Sleep(1.0);
     driveToWall(30);
     Sleep(1.0);
-	move_backwards(30, 2);
+    move_backwards(30,1);
     turn_left(20, 90);
-    left_motor.SetPercent(30);
-    right_motor.SetPercent(30);
-    while(middle.Value() > 1.2);
-    left_motor.Stop();
-    right_motor.Stop();
-    turn_right(20,135);
-    followLine(10);
+    Sleep(1.0);
+    move_forward(30, 18);
+    Sleep(1.0);
+    turn_right(20,90);
+    Sleep(1.0);
+    move_forward(30, 15);
+    if(getLightColor == 1) {
+        LCD.SetBackgroundColor(LCD.Blue);
+    }
+    else if(getLightColor == 0) {
+        LCD.SetBackgroundColor(LCD.Red);
+    }
+    else {
+        LCD.WriteLine("No light detected");
+    }
 }
 
 int main(void)
 {
-	waitForStart();
+    waitForStart();
     performance1();
+
+
 
     return 0;
 }
