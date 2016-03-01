7e6a502a0ad4eac0be984e2680dbb7425aa69c59
nishadprabhu <prabhunishad@yahoo.com>
2016-03-01 15:23:24 -0500

    Added files via upload
    Location class; contains x and y coordinates for many locations of interest on the course

---
+++ locations.h
@@ -0,0 +1,35 @@
+#ifndef LOCATION_H
+#define LOCATION_H
+
+/**
+ * This is a class which holds the X and Y coordinates for all locations of interest on the 2016 FEH Robot course.
+ */
+class Robot
+{
+	public:
+		static const float SUPPLIES_X = x;
+		static const float SUPPLIES_Y = x;
+		
+		static const float BOTTOM_SIDE_RAMP_X = x;
+		static const float BOTTOM_SIDE_RAMP_Y = x;
+		
+		static const float TOP_SIDE_RAMP_X = x;
+		static const float TOP_SIDE_RAMP_Y = x;
+		
+		static const float FUEL_LIGHT_X = x;
+		static const float FUEL_LIGHT_Y = x;
+		
+		static const float MID_SWITCH_X = x;
+		static const float MID_SWITCH_Y = x;
+		
+		static const float RIGHT_SWITCH_X = x;
+		static const float RIGHT_SWITCH_Y = x;
+		
+		static const float LEFT_SWITCH_X = x;
+		static const float LEFT_SWITCH_Y = x;
+		
+		static const float START_X = x;
+		static const float START_Y = x;
+};
+
+#endif
