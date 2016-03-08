ee78e69b6ab9e2c6d8209ce2cd02a6e469c40ec8
nishadprabhu <prabhunishad@yahoo.com>
2016-03-08 10:31:16 -0500

    Update locations.h

--- locations.h
+++ locations.h
@@ -1,35 +1,36 @@
-#ifndef LOCATION_H
-#define LOCATION_H
+#ifndef LOCATIONS_H
+#define LOCATIONS_H
 
 /**
  * This is a class which holds the X and Y coordinates for all locations of interest on the 2016 FEH Robot course.
  */
 class Location
 {
-	public:
-		static const float SUPPLIES_X = x;
-		static const float SUPPLIES_Y = x;
-		
-		static const float BOTTOM_SIDE_RAMP_X = x;
-		static const float BOTTOM_SIDE_RAMP_Y = x;
-		
-		static const float TOP_SIDE_RAMP_X = x;
-		static const float TOP_SIDE_RAMP_Y = x;
-		
-		static const float FUEL_LIGHT_X = x;
-		static const float FUEL_LIGHT_Y = x;
-		
-		static const float MID_SWITCH_X = x;
-		static const float MID_SWITCH_Y = x;
-		
-		static const float RIGHT_SWITCH_X = x;
-		static const float RIGHT_SWITCH_Y = x;
-		
-		static const float LEFT_SWITCH_X = x;
-		static const float LEFT_SWITCH_Y = x;
-		
-		static const float START_X = x;
-		static const float START_Y = x;
+    public:
+        static const float SUPPLIES_X = 29.35;
+        static const float SUPPLIES_Y = 12.3;
+
+        static const float BOTTOM_SIDE_RAMP_X = 28.1;
+        static const float BOTTOM_SIDE_RAMP_Y = 22.9;
+
+        static const float TOP_SIDE_RAMP_X = 33.1;
+        static const float TOP_SIDE_RAMP_Y = 50;
+
+       static const float BOT_MAIN_RAMP_X = 29.1;
+       static const float BOT_MAIN_RAMP_Y = 24.3;
+
+        static const float TOP_MAIN_RAMP_X = 29.4;
+        static const float TOP_MAIN_RAMP_Y = 44.3;
+
+
+        static const float FUEL_LIGHT_X = 30.5;
+        static const float FUEL_LIGHT_Y = 61.6;
+
+        static const float MID_SWITCH_X = 6.5;
+        static const float MID_SWITCH_Y = 46.1;
+
+        static const float START_X = 7.6;
+        static const float START_Y = 8.9;
 };
 
 #endif
